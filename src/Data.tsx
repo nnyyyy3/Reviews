f672c9ff70fc82ca9ab72e97cf8e12167c2484cd
nnyy <angelnina.doyohim.21@usjr.edu.ph>
2023-10-12 17:52:14 +0800

    first commit

---
+++ src/Data.tsx
@@ -0,0 +1,30 @@
+export default[
+    {
+        ID: 1,
+        pic: 	"https://course-api.com/images/people/person-1.jpeg",
+        author: "Susan Smith",
+        job: "WEB DEVELOPER",
+        info: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everday carry."
+    },
+    {
+        ID: 2,
+        pic: "https://course-api.com/images/people/person-2.jpeg",
+        author: "Anna Johnson",
+        job: "WEB DESIGNER",
+        info:"Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+        ID:3,
+        pic: "https://course-api.com/images/people/person-4.jpeg",
+        author: "Peter Jones",
+        job: "INTERN",
+        info:"Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+    {
+        ID:4,
+        pic: "https://course-api.com/images/people/person-3.jpeg",
+        author: "Bill Anderson",
+        job: "THE BOSS",
+        info:"Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic."
+    }
+]
